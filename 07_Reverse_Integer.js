cb7bc0b989f1eaeae7fb925d69270e9029384b49
skyying <dreamuk@gmail.com>
2018-03-18 23:47:47 +0800

    reverse a 32 bit signed integer without overflow

---
+++ 07_Reverse_Integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+
+    var ans = "",
+        isNegtive = x < 0;
+
+    x = x > 0 ? x + "" : (x + "").slice(1);
+    for (var i = x.length - 1; i >= 0; i--) {
+        ans += x[i];
+    }
+
+    ans = (isNegtive ? "-" : "") + ans;
+    return +ans > Math.pow(2, 32) || +ans < -Math.pow(2, 32)  ? +ans :  0;
+};
+
+console.log(reverse(-123))
+console.log(reverse(1230))
