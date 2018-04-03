31f3f47a488a23d3f354a617c05de98f6b989c99
Chien Wen Liu <dreamuk@gmail.com>
2018-04-03 15:16:54 +0800

    add comment

--- 398_Random_Pick_Index.py
+++ 398_Random_Pick_Index.py
@@ -13,12 +13,16 @@ class Solution:
         :type target: int
         :rtype: int
         """
+        # Store target's index in an array
         match_index = []
         for i in range(len(self.arr)):
             if self.arr[i] == target:
                 match_index.append(i)
 
+        # Generate a random index in this matched array
         rnd_index = randint(0, len(match_index)-1)
+
+        # return matched index
         return match_index[rnd_index]
 
 
