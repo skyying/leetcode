b97974324f55bf25a9d72feb4be84b3c282e59a5
Chien Wen Liu <dreamuk@gmail.com>
2018-04-06 20:10:21 +0800

    fix wrong solutions

--- 21_Merge_Two_Sorted_Lists.js
+++ 21_Merge_Two_Sorted_Lists.js
@@ -11,27 +11,83 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-  //  if( l1.length || l2.length ) return l1.concat(l2);
-    let i=0, j=0, arr=[];
    
-    while( i < l1.length  ||  j < l2.length )
-    {
-        if(!l1[i] || !l2[j] ){
-           arr.push(l1[i] || l2[j]);
-           i++;
-           j++; 
-        }else{
-            if( l1[i] <= l2[j] || !l2[i]){
-                arr.push(l1[i]);
-                i++;
-            }else{
-                arr.push(l2[j]);
-                j++;
-            }
-        }
+    
+    var newList = new ListNode(null);
+    var head = newList;
+    if(!l1 || !l2) {
+        return l1 || l2;
     }
-    return arr;
+
+    while(l1 || l2){
+        if( (l1 && l2 &&  l1.val < l2.val) || !l2 ){
+            newList.next = new ListNode(l1.val);
+            l1 = l1.next;
+        }else if( (l1 && l2 && l1.val >= l2.val) || !l1){
+            newList.next = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+        newList = newList.next;
+    }
+    return head.next;
+    
 };
 
 
-console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]))
+//Object - Node
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+ }
+
+
+ // testcase
+function Test_mergeTwoLists(testArray1, testArray2, expectedArray){
+
+    this.testArray1 = testArray1;
+    this.testArray2 = testArray2;
+    this.expectedArray = expectedArray;
+    this.arrayToList = function(arr){
+        var head = new ListNode(arr[0]);
+        var current = head;
+        
+        for(var i=1; i<arr.length; i++){
+            var newNode = new ListNode(arr[i]);
+            current.next = newNode;
+            current = current.next;
+        }
+        return head;
+    };
+
+    this.listToArray = function(list){
+        var current = list;
+        var arr = [], i=0;
+        while(current){
+            arr[i] = current.val;
+            current = current.next;
+            i++;
+        }
+
+        return arr;
+    };
+    
+    this.valdate = function(){
+
+        var l1 = this.arrayToList(this.testArray1);
+        var l2 = this.arrayToList(this.testArray2);
+        var testedList = mergeTwoLists(l1, l2);
+        var result = this.listToArray(testedList);
+        if(result.length !== expectedArray.length)
+            return false;
+        return result.every((val, i) => val === expectedArray[i]);
+        
+    };
+    this.isEqual = this.valdate();
+}
+
+//var test01 = new Test_removeNthFromEnd([1, 1], [1], 1);
+var test01 = new Test_mergeTwoLists([1, 3, 5], [1, 5], [1, 1, 3, 5, 5]);
+var test02 = new Test_mergeTwoLists([1, 1], [1, 3],[ 1, 1, 1, 3] );
+//console.log(test01.isEqual);
+console.log(test01.isEqual);
+console.log(test02.isEqual);
