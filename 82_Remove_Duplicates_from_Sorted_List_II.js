b781a6ebb4efe735aa53b280e6fb622e6ba75681
Chien Wen Liu <dreamuk@gmail.com>
2018-04-06 15:34:28 +0800

    add ideas to comments

--- 82_Remove_Duplicates_from_Sorted_List_II.js
+++ 82_Remove_Duplicates_from_Sorted_List_II.js
@@ -6,10 +6,17 @@
  * }
  */
 /**
+ * 題目: 移除一個排序過的鏈表中重複的元素，eg, 1->1->2->3 需要回傳 2->3;
+ * Idea: 1. 新增一個鏈表，把比較後的結果傳回新鏈表。
+ *       2. 逐一對鏈表中的元素(i, i+1) 兩兩相比，如果相同，這兩個元素皆不會被放入新鏈表，並從i+2個元素開始相比
+ *       3. i+2 有可能跟i 相同, 因此要記住前次比較相同的值k ，並將k與i+2比較, 若相同, i+2也不會被加入；
+ * 
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
+
+
+ var deleteDuplicates = function(head) {
 
     var current = head, lastSameValue;
     var newHead = new ListNode();
