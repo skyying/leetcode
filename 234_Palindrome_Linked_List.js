54c86b5a245f8d8ad88c611b18549530a75ae52d
Chien Wen Liu <dreamuk@gmail.com>
2018-04-10 11:48:54 +0800

    add ideas

--- 234_Palindrome_Linked_List.js
+++ 234_Palindrome_Linked_List.js
@@ -6,6 +6,12 @@
  * }
  */
 /**
+ * 題目 : 檢查一個list 是不是Palindrome
+ * idea 1: 第一個想到的想法是一樣切成兩個list, 反轉第二個list,再與第一list進行比較, 奈何這樣效率佷差。
+ * idea 2: 除了reverse list可以得到反轉的排序之外, 也可以使用多增加一個prev properity來做，記得每個node的
+ * 前一個是什麼。因此當從頭開始, 最一開始的node的prev值是null, 下一個的prev 是現在這個, 逐步loop到底時, 可以
+ * 利用prev屬性回到前一個，因此prev屬性最終的目的是爲了讓我們可以從後面loop回來, 因此我們就可以跑一次迴圈, 
+ * 同時比較從頭開始的node跟從後面loop回來的node, 假設他們自始至終都相同，那麼就可以確認是Palindrome
  * @param {ListNode} head
  * @return {boolean}
  */
@@ -31,37 +37,7 @@ var isPalindrome = function(head) {
     }
 
     return true;
-    // while(fast.next && fast.next.next){
-    //     slow = slow.next;
-    //     fast = fast.next.next;
-    // }
-
-    // function reverseList(list){
-    //     var curr = list;
-    //     var revesed = null;
-    //     while(curr){
-    //         var nxt = curr.next;
-    //         curr.next = revesed;
-    //         revesed = curr;
-    //         curr = nxt;
-    //     }
-    //     return revesed;
-    // }
-    // var l1= head;
-    // var l2 = reverseList(slow.next);
-    // slow.next = null;
    
-    // while(l1 && l2){
-    //     if(l1.val !== l2.val){
-    //         return false;
-    //     }
-    //     l1 = l1.next;
-    //     l2 = l2.next;
-    // }
-
-   
-
-    // return true;
   
 };
 
@@ -95,5 +71,5 @@ listToArray = function(list){
     return arr;
 };
 
-var a = arrayToList([1, 4, 3, 4, 1]);
-console.log(isPalindrome(a));
+var list = arrayToList([1, 4, 3, 4, 1]);
+console.log(isPalindrome(list));
