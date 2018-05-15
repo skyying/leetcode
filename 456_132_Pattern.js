0447e9f3e0f5ec662195aaca0bac8238d9e8c8ff
Chien Wen Liu <dreamuk@gmail.com>
2018-05-15 14:26:40 +0800

    soluttion o(n)

--- 456_132_Pattern.js
+++ 456_132_Pattern.js
@@ -1,7 +1,8 @@
 /**
  * @param {number[]} nums
  * @return {boolean}
- */
+ */ 
+//solution 1 o(n^2)
 var find132pattern = function(nums) {
 
     let left = nums[0], center;
@@ -19,5 +20,29 @@ var find132pattern = function(nums) {
     return false;
 
 };
+
+
+// solution 2, o(n)
+
+var find132pattern = function(nums){
+
+    let min = [nums[0]], stack = [];
+
+    for(let i = 1; i < nums.length; i++){
+        min[i] = Math.min(min[i-1], nums[i]);
+    }
+    for(let j = nums.length - 1; j > 0 ; j--){
+        if(nums[j] > min[j]){
+            while(stack.length > 0 && stack[stack.length -1] <= min[j]){
+                stack.pop();
+            }
+            if(stack.length > 0 && stack[stack.length -1] < nums[j] ) {
+                return true; 
+            }
+            stack.push(nums[j]);
+        }
+    }
+    return false
+}
 
 console.log(find132pattern([3, 1, 4, 2]));
