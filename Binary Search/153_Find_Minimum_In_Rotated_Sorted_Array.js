1a2c1f3632c5c15bc73bb47c2700fbadc2822e96
Chien Wen Liu <dreamuk@gmail.com>
2018-05-19 18:25:49 +0800

    arrange comments

--- Binary Search/153_Find_Minimum_In_Rotated_Sorted_Array.js
+++ Binary Search/153_Find_Minimum_In_Rotated_Sorted_Array.js
@@ -1,7 +1,9 @@
 /**
  * @param {number[]} nums
  * @return {number}
- * idea: 雖然可以透過liner search來找出最小的，不過還是可以用binary search來找出最小的值。主要的挑戰在怎麼決定往兩邊的那邊找，這裏我想到的方法是，比較mid, low, high, 三個值最小，如果是high位置的值比low, mid小，那麼表示最小的值一定在mid + 1 ~ high這個範圍，此時重設low的值即可。
+ * idea: 雖然可以透過liner search來找出最小的，不過還是可以用binary search來找出最小的值。
+ * 主要的挑戰在怎麼決定往兩邊的那邊找，這裏我想到的方法是，比較mid, low, high, 三個值最小，
+ * 如果是high位置的值比low, mid小，那麼表示最小的值一定在mid + 1 ~ high這個範圍，此時重設low的值即可。
  */
 var findMin = function(nums) {
 
