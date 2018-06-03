0b78b3699bb90e4a7c55e01deabb3641c8308178
Chien Wen Liu <dreamuk@gmail.com>
2018-06-03 12:14:05 +0800

    add solution

---
+++ 255_implement_stack_usng_queue.js
@@ -0,0 +1,45 @@
+var MyStack = function() {
+    this.queue = [];
+};
+
+/**
+ * Push element x onto stack. 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.push(x);
+};
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    this.queue.pop();
+};
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue[this.queue.length - 1]
+};
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue.length === 0;
+};
+
+
+
+
+
+
+
+
+
