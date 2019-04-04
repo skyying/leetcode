774a6f24200f3b1fa04fe71921e8513ede08b88c
Chien Wen Liu <dreamuk@gmail.com>
2019-04-04 09:21:47 +0800

    add solution

--- 438.rb
+++ 438.rb
@@ -1,32 +1,35 @@
 # @param {String} s
 # @param {String} p
 # @return {Integer[]}
+# Find All Anagrams in a String
+# using sliding window, and use two array to keep tracking current sub-string
 def find_anagrams(s, p)
   return [] if p.size == 0 || s.size == 0 || s.size < p.size
-  
-  slen = s.size
-  plen = p.size
-  startIndices = []
-  arr_s = Array.new(3, 0)
-  arr_p = Array.new(3, 0)
+
+  s_len = s.size
+  p_len = p.size
+  s_arr = Array.new(26, 0)
+  p_arr = Array.new(26, 0)
+  a_ord = 97
+  right = 0
+  ans = []
 
   p.each_byte do |c|
-    arr_p[c - 97] += 1
+    p_arr[c - a_ord ] += 1
   end
+  p p_arr
 
-  right = 0
-        
-  (0...slen).each do |left|
-    while right < slen && (right - left + 1) <= plen 
-      arr_s[(s[right]).bytes.first - 97] += 1
-      right +=1 if right < slen
+  (0...s_len).each do |left|
+    while right < s_len && (right - left + 1) <= p_len
+      s_arr[(s[right].ord - a_ord)] += 1
+      right += 1
     end
-    if arr_s == arr_p
-      startIndices << left 
+    if s_arr == p_arr
+      ans << left
     end
-    arr_s[(s[left]).bytes.first - 97] -= 1
+    s_arr[s[left].ord - a_ord] -= 1
   end
-  return startIndices
+  ans
 end
         
-p find_anagrams("baa", "aa") == [1]
+p find_anagrams("baaaa", "aa")
